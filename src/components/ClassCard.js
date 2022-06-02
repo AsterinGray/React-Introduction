921b1ceb53678e8c5f72ff636ebd3ba5e1cd70b2
AsterinGray <raymondtiandinatan@gmail.com>
2022-06-02 22:04:23 +0700

    functional and class component

---
+++ src/components/ClassCard.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class ClassCard extends Component {
+  render() {
+    return <h1>Class Card</h1>;
+  }
+}
+
+export default ClassCard;
---
+++ src/components/FunctionalCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FunctionalCard = () => {
+  return <h1>Functional Card</h1>;
+};
+
+export default FunctionalCard;
